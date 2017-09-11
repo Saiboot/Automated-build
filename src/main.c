1e141e22dc6ed6e82d05c305dc624342a79269aa
Tobias Ljung <t3eg3e@gmail.com>
2017-09-11 07:16:26 +0200

    Create main.c

---
+++ src/main.c
@@ -0,0 +1,15 @@
+#include <SDL2/SDL.h>
+#include <stdio.h>
+
+int main(int argc, char **argv)
+{
+  if(SDL_Init(SDL_INIT_VIDEO) != 0) {
+    printf("SDL initialization failed!");
+    return -1;
+  }
+  
+  printf("SDL initialization succeeded!");
+  SDL_Quit();
+  
+  return 0;
+}
